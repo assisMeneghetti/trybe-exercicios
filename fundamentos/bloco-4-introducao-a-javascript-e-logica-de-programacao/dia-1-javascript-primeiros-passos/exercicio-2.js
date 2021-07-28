3d22172e10430a56f70bd99a245dca3f5c12ab45
assisMeneghetti <assismfabri@hotmail.com>
2021-07-28 19:04:29 -0300

    Adiciona exercicios de 2 ao 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-2.js
@@ -0,0 +1,6 @@
+let smallerNumber = 5;
+let largerNumber = 25;
+
+if (largerNumber > smallerNumber) {
+  console.log(largerNumber);
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercise-4.js
@@ -0,0 +1,11 @@
+let firstNumber = 30;
+let secondNumber = 9;
+let thirdNumber = 21;
+
+if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+  console.log(firstNumber);
+} else if (secondNumber > thirdNumber) {
+  console.log(secondNumber);
+} else {
+  console.log(thirdNumber);
+}
