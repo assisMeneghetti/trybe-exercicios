07c38395e73fb1d25089d31612c280834b697eea
assisMeneghetti <assismfabri@hotmail.com>
2021-07-28 18:49:18 -0300

    Adiciona exercicios de fixaçao e exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/fixacao-2.js
@@ -0,0 +1,13 @@
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(isEnrolled);
+console.log(isEnrolled);
+console.log(patientEmail);
+//console.log(patientAge);
