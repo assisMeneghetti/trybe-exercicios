d296c929e43704bdc9badfe642736f49fba3c16c
assisMeneghetti <assismfabri@hotmail.com>
2021-08-02 13:40:31 -0300

    Adiciona exercicio 8

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercise-7.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercise-7.js
@@ -4,7 +4,7 @@
  */
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let smallerNumber = numbers[0];
-for (i = 0; i < numbers.length; i += 1) {
+for (let i = 0; i < numbers.length; i += 1) {
   if (smallerNumber > numbers[i]) {
     smallerNumber = numbers[i];
   }
