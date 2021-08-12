daea74d6888b0e032a454c5c2745ab84feb5436a
assisMeneghetti <assismfabri@hotmail.com>
2021-08-12 08:45:00 -0300

    add exercise 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercise-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercise-1.js
@@ -12,9 +12,9 @@
 
 let lado = 5;
 const asterisco = '*';
-let quadrado = '';
 
 for (let qtdLinha = 0; qtdLinha < lado; qtdLinha += 1) {
+  let quadrado = '';
   for (let qtdColuna = 0; qtdColuna < lado; qtdColuna += 1) {
     quadrado += asterisco;
   }
