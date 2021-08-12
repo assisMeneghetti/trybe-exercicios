07e3c3507c5bd52b88191d8d51468369bbba07f3
assisMeneghetti <assismfabri@hotmail.com>
2021-08-12 08:20:55 -0300

    some changes in exercise 4.2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercise-2.js
@@ -0,0 +1,10 @@
+let lado = 5;
+const asterisco = '*';
+let quadrado = '';
+
+for (let qtdLinha = 0; qtdLinha < lado; qtdLinha += 1) {
+  // for (let qtdColuna = 0; qtdColuna < lado; qtdColuna += 1) {
+  quadrado += asterisco;
+  console.log(quadrado);
+  // }
+}
--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -15,12 +15,39 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 //#1
+// const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+// let ul = document.querySelector('#days');
+// function creatDays(array, tag) {
+//   for (let index = 0; index < dezDaysList.length; index += 1) {
+//     let li = document.createElement('li');
+//     li.innerText = dezDaysList[index];
+//     li.className = 'day';
+//     ul.appendChild(li);
+//   }
+// }
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 let ul = document.querySelector('#days');
 
-for (let index = 0; index < dezDaysList.length; index += 1) {
-  let li = document.createElement('li');
-  li.innerText = dezDaysList[index];
-  li.className = 'day';
-  ul.appendChild(li);
+function creatDays(array, tag, pai) {
+  for (let index = 0; index < array.length; index += 1) {
+    document.createElement(tag);
+    pai.appendChild(tag);
+  }
 }
+
+creatDays(dezDaysList, 'li', ul);
+
+function setClass(array, classe) {
+  for (let index = 0; index < array.length; index += 1) {
+    tag.className = classe;
+  }
+}
+
+let classe = 'day';
+
+setClass(dezDaysList, classe)
+
+
+
+creatDays(dezDaysList, ul)
+//#2
