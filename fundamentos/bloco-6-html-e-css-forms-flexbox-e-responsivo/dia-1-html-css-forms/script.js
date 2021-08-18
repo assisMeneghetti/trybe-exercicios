e0208b0ca8719d25a7630ff0c885ee9be5edd278
assisMeneghetti <assismfabri@hotmail.com>
2021-08-18 17:35:10 -0300

    adiciona parte 2 do exercicio

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,23 +1,18 @@
+
 let stateList = document.getElementById("state-list");
 let statesNames = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
 let acronyms = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"];
 
-for (let index = 0; index < 10; index += 1) {
-  let option = document.createElement("option");
-  option.innerText = statesNames[index];
-  option.value = acronyms[index];
-  stateList.appendChild(option);
+function createStateLists() {
+
+  for (let index = 0; index < 10; index += 1) {
+    let option = document.createElement("option");
+    option.innerText = statesNames[index];
+    option.value = acronyms[index];
+    stateList.appendChild(option);
+  }
 }
 
+createStateLists();
 
-// let ulContent = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
-//     for (let index = 0; index < ulContent.length; index += 1) {
-//       let ul = document.createElement('ul');
-//       ul.innerText = ulContent[index];
-//       section3.appendChild(ul);
-//     }
-// for (let state of statesNames) {
-//   console.log(state);
-//   stateList.appendChild(option);
-//   option.innerText = state;
-// }
+let inputDate = document.getElementById('date').innerText;
