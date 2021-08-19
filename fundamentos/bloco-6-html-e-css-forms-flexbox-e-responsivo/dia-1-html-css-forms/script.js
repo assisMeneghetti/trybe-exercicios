fee891bb457253db55c733614bc888e5069ad177
assisMeneghetti <assismfabri@hotmail.com>
2021-08-19 12:09:18 -0300

    adiciona parte 3 do exercicio

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,5 +1,3 @@
-
-
 function createStateLists() {
   let stateList = document.getElementById("state-list");
   let statesNames = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
@@ -16,25 +14,6 @@ function createStateLists() {
 createStateLists();
 
 let button = document.querySelector("#submit-form");
-
-// button.addEventListener('click', function (event) {
-//   let date = document.getElementById('date').value;
-//   event.preventDefault();
-//   let day = [];
-//   let month = [];
-//   let year = [];
-//   for (let index = 0; index < date.length; index += 1) {
-//     if (index > 2 && index < 5) {
-//       month.push(parseInt(date[index]));
-//     } else if (index < 2) {
-//       day.push(parseInt(date[index]));
-//     } else if (index > 5) {
-//       year.push(parseInt(date[index]));
-//     }
-//   }
-//   console.log(day, month, year);
-// })
-
 
 button.addEventListener('click', function (event) {
   event.preventDefault();
@@ -64,17 +43,41 @@ button.addEventListener('click', function (event) {
   } else if (month < 0 || month > 12) {
     alert('Mês formato errado!')
   } else if (year < 0) {
-    'Ano no formato errado!'
+    alert('Ano no formato errado!');
   }
 })
 
+/**Sourse: https://www.w3schools.com/js/tryit.asp?filename=tryjs_form_elements*/
+let form = document.querySelector('#form');
 
-// verifica o tamanho
-// separa string .split([separator[, limit]]) retorna um array
-// parseInt(string)
+button.addEventListener('click', function (event) {
 
+  let text = "";
 
+  for (let index = 0; index < form.length; index += 1) {
+    text += form.elements[index].value + '<br>';
+  }
+  document.getElementById('result').innerHTML = text;
+});
 
+/**Source: https://www.w3schools.com/js/tryit.asp?filename=tryjs_validation_js */
+button.addEventListener('click', function (event) {
 
-// let inputDate = document.getElementById('date').innerText;
+  let inputs = document.querySelectorAll('input');
+  let ids = [];
+
+  for (let input of inputs) {
+    ids.push(input.id);
+  }
+
+  for (let index = 0; index < ids.length; index += 1) {
+
+    let valid = document.forms['form'][ids[index]].value;
+    if (valid === '') {
+      document.getElementById('result').innerHTML = "Todos os campos são obrigatório!";
+      return false;
+    }
+
+  }
+})
 
