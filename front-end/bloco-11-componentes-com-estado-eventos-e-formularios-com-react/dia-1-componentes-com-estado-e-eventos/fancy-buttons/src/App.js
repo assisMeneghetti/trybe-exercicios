6c0c6298a92d7a867df86ed799f1d3fa0c72cc3f
assisMeneghetti <assismfabri@hotmail.com>
2021-10-07 18:56:05 -0300

    fancy-button trabalhando com stats

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import './App.css';
 
-function handleClickLeftBtn() {
-  console.log('Botão da esquerda');
-}
-
-function handleClickCenterBtn() {
-  console.log('Botão do meio');
-}
-
-function handleClickRightBtn() {
-  console.log('Botão da direita');
-}
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      clicksLeftBtn: 0,
+      clicksCenterBtn: 0,
+      clicksRightBtn: 0,
+    }
+
+    this.handleClickLeftBtn = this.handleClickLeftBtn.bind(this)
+    this.handleClickRightBtn = this.handleClickRightBtn.bind(this)
+    this.handleClickCenterBtn = this.handleClickCenterBtn.bind(this)
+  }
+
+  handleClickLeftBtn = () => {
+    /* Passando uma callback à função setState, que recebe de parâmetros
+    o estado anterior e as props do componente, você garante que as atualizações
+    do estado acontecerão uma depois da outra! */
+    this.setState((previousState, _props) => ({
+      clicksLeftBtn: previousState.clicksLeftBtn + 1
+    }))
+  }
+
+  handleClickCenterBtn = () => {
+    this.setState((previousState, _props) => ({
+      clicksCenterBtn: previousState.clicksCenterBtn + 1
+    }))
+  }
+
+  handleClickRightBtn = () => {
+    this.setState((previousState, _props) => ({
+      clicksRightBtn: previousState.clicksRightBtn + 1
+    }))
+  }
+
+
   render () {
     return (
       <div className="btn-container">
-        <button onClick={handleClickLeftBtn}>Esqueda</button>
-        <button onClick={handleClickCenterBtn}>Centro</button>
-        <button onClick={handleClickRightBtn}>Direita</button>
+        <button className="btn" onClick={this.handleClickLeftBtn}>{this.state.clicksLeftBtn}</button>
+        <button className="btn" onClick={this.handleClickCenterBtn}>{this.state.clicksCenterBtn}</button>
+        <button className="btn" onClick={this.handleClickRightBtn}>{this.state.clicksRightBtn}</button>
       </div>
     );
   }
