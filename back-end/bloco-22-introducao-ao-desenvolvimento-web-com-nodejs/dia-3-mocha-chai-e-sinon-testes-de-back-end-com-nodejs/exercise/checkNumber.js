0b4a8feb3db977ad708874b3297a962cb97cac44
assisMeneghetti <assismfabri@hotmail.com>
2022-06-09 10:12:17 -0300

    feat: create checkNumber function

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercise/checkNumber.js
@@ -0,0 +1,14 @@
+const checkNumber = (number) => {
+  if (typeof number !== 'number') return "o valor deve ser um número";
+  if (number < 0) {
+    return 'negativo'
+  } else if (number > 0) {
+    return 'positivo'
+  } else if (number === 0) {
+    return 'neutro';
+  }
+};
+
+// console.log(checkNumber(0));
+
+module.exports = checkNumber;
