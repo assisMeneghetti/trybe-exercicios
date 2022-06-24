78043f3c941af28e6f4ede93d623726b0f0340ec
assisMeneghetti <assismfabri@hotmail.com>
2022-06-24 19:20:25 -0300

    feat: add get all users controller

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercise_model/controllers/getAllUsers.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercise_model/controllers/getAllUsers.js
@@ -1,9 +1,10 @@
 const model = require('../model/user');
+
 module.exports = async (req, res, next) => {
   try {
     const users = await model.getAll();
     return res.status(200).json(users);
   } catch (error) {
-    next(e);
-  };
+    next(error);
+  }
 };
