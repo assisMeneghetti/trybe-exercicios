8a633dc4dbe9d718de251368de720f942f40684b
assisMeneghetti <assismfabri@hotmail.com>
2022-06-24 19:23:42 -0300

    feat: export middlewares by index

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercise_model/middlewares/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercise_model/middlewares/index.js
@@ -3,6 +3,7 @@ const validateEmail = require('./validateEmail');
 const validateFirstName = require('./validateFirstName');
 const validateLastName = require('./validateLastName');
 const validatePassword = require('./validatePassword');
+const routeNotFound = require('./routeNotFound');
 
 module.exports = {
   handleError,
@@ -10,4 +11,5 @@ module.exports = {
   validateFirstName,
   validateLastName,
   validatePassword,
+  routeNotFound,
 };
