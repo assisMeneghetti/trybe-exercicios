418ee2218d2bd00a8fbe496a917cb1620966983d
assisMeneghetti <assismfabri@hotmail.com>
2022-06-24 19:21:51 -0300

    feat: add validate password middleware

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercise_model/middlewares/validatePassword.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercise_model/middlewares/validatePassword.js
@@ -1,8 +1,18 @@
 module.exports = (req, res, next) => {
   try {
     const { password } = req.body;
-    if (!password) return res.status(400).json({ message: "O campo 'password' é obrigatório"});
-    if (password.length < 6) return res.status(400).json({ message: "O campo 'password' deve ter pelo menos 6 caracteres"});
+    if (!password) {
+      return res.status(400).json({ 
+        error: true,
+        message: 'O campo \'password\' é obrigatório',
+      }); 
+}
+    if (password.length < 6) {
+      return res.status(400).json({
+        error: true,
+        message: 'O campo \'password\' deve ter pelo menos 6 caracteres',
+      }); 
+}
     return next();
   } catch (e) {
     return next(e);
