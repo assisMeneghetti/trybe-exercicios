861e63b200a40b9418d04a009bea961c6740b88b
assisMeneghetti <assismfabri@hotmail.com>
2022-06-24 19:23:06 -0300

    chore: export controllers by index

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercise_model/controllers/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercise_model/controllers/index.js
@@ -1,7 +1,11 @@
 const createUser = require('./createUser');
 const getAllUsers = require('./getAllUsers');
+const getUserById = require('./getUserById');
+const updateUser = require('./updateUser');
 
 module.exports = { 
   getAllUsers,
   createUser,
+  getUserById,
+  updateUser,
 };
