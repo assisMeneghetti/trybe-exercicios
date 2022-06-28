308928f498970f91af1b796c186f90eabaa9cdd3
assisMeneghetti <assismfabri@hotmail.com>
2022-06-28 12:56:55 -0300

    refactor: refactoring to make it RESTfull

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercise/controllers/productController.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercise/controllers/productController.js
@@ -2,39 +2,62 @@ const express = require('express');
 const ProductModel = require('../models/productModel');
 
 const router = express.Router();
+const notFoundMsg = 'Product not found';
 
-router.get('/list-products', async (req, res) => {
+router.get('/', async (req, res) => {
   const products = await ProductModel.getAll();
-
-  res.send(products);
+  console.log('products', products);
+  res.status(200).json(products);
 });
 
-router.get('/get-by-id/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  if (!product) {
+    res.status(404).json({ message: notFoundMsg });
+  }
+
+  res.status(200).json(product);
 });
 
-router.post('/add-product', async (req, res) => {
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
+
+  if ([name, brand].includes(undefined)) {
+    res.status(400).json({ message: 'Missing required fields' });
+  }
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  res.status(201).json(newProduct);
 });
 
-router.post('/delete-product/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
+router.delete('/:id', async (req, res) => {
+  const product = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+  if (!product) {
+    res.status(404).json({ message: notFoundMsg });
+  }
+  
+  res.status(204).json();
 });
 
-router.post('/update-product/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
-  const products = await ProductModel.update(req.params.id, name, brand);
+  if ([name, brand].includes(undefined)) {
+    res.status(400).json({ message: 'Missing required fields' });
+  }
 
-  res.send(products);
+  const product = await ProductModel.getById(req.params.id);
+
+  if (!product) {
+    res.status(404).json({ message: notFoundMsg });
+  }
+
+  await ProductModel.update(req.params.id, name, brand);
+
+  res.status(200).json({ name, brand });
 });
 
 module.exports = router;
