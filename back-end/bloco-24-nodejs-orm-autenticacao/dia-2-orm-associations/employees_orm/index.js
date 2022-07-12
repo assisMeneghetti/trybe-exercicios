001fa1d8787489c2e7f72759c9c6c219a2ce3785
assisMeneghetti <assismfabri@hotmail.com>
2022-07-12 13:11:21 -0300

    feat: create main index

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/employees_orm/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/employees_orm/index.js
@@ -1,8 +1,14 @@
 // index.js
 const express = require('express');
+const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
 const { Address, Employee } = require('./src/database/models');
+const config = require('./src/database/config/config');
 
 const app = express();
+app.use(bodyParser.json());
+
+const sequelize = new Sequelize(process.env.NODE_ENV === 'test' ? config.test : config.development);
 
 app.get('/employees', async (_req, res) => {
   try {
@@ -18,17 +24,37 @@ app.get('/employees', async (_req, res) => {
 });
 
 // Eager Loading 1:N
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({
+        where: { id },
+          include: [{
+            model: Address, as: 'addresses', attributes: { exclude: ['number'] },
+          }],
+      });
+
+    if (!employee) { return res.status(404).json({ message: 'Funcionário não encontrado' }); }
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+// Lazy Loading 1:N
 // app.get('/employees/:id', async (req, res) => {
 //   try {
 //     const { id } = req.params;
-//     const employee = await Employee.findOne({
-//         where: { id },
-//           include: [{
-//             model: Address, as: 'addresses', attributes: { exclude: ['number'] },
-//           }],
-//       });
+//       const employee = await Employee.findOne({ where: { id } });
 
 //     if (!employee) { return res.status(404).json({ message: 'Funcionário não encontrado' }); }
+
+//       if (req.query.includeAddresses === 'true') {
+//         const addresses = await Address.findAll({ where: { employeeId: id } });
+//         return res.status(200).json({ employee, addresses });
+//       }
 
 //     return res.status(200).json(employee);
 //   } catch (e) {
@@ -37,25 +63,66 @@ app.get('/employees', async (_req, res) => {
 //   }
 // });
 
-// Lazy Loading 1:N
-app.get('/employees/:id', async (req, res) => {
+// Unmanaged transactions
+app.post('/employees', async (req, res) => {
+  // Primeiro iniciamos a transação
+  const t = await sequelize.transaction();
+
   try {
-    const { id } = req.params;
-      const employee = await Employee.findOne({ where: { id } });
+    const { firstName, lastName, age, city, street, number } = req.body;
 
-    if (!employee) { return res.status(404).json({ message: 'Funcionário não encontrado' }); }
+    // Depois executamos as operações
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
 
-      if (req.query.includeAddresses === 'true') {
-        const addresses = await Address.findAll({ where: { employeeId: id } });
-        return res.status(200).json({ employee, addresses });
-      }
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
 
-    return res.status(200).json(employee);
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+
+    return res.status(201).json({ id: employee.id, message: 'Cadastrado com sucesso' });
   } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    await t.rollback();
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
   }
 });
+
+// Managed transactions
+// app.post('/employees', async (req, res) => {
+//   try {
+//     const { firstName, lastName, age, city, street, number } = req.body;
+
+//     const result = await sequelize.transaction(async (t) => {
+//       const employee = await Employee.create({
+//         firstName, lastName, age,
+//       }, { transaction: t });
+
+//       await Address.create({
+//         city, street, number, employeeId: employee.id,
+//       }, { transaction: t });
+
+//       return res.status(201).json(result);
+//     });
+
+//     // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+//     // não sendo necessário finalizar a transação manualmente.
+//     // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
+//   } catch (e) {
+//     // Se entrou nesse bloco é porque alguma operação falhou.
+//     // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+//     console.log(e.message);
+//     res.status(500).json({ message: 'Algo deu errado' });
+//   }
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
